26ae7e98eeb4321d78419ec7c4a6da191d03b406
Pablo Fernandez Grado <pablo93fernandez@gmail.com>
2024-11-21 16:55:18 +0100

    feat: se añade websocket para ver el plano en varios dispositivos a la vez con actualizaciones en tiempo real

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+import axios from "axios";
 import cors from "cors";
+import "dotenv/config";
 import express, { json } from "express";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
 import { GrupoModel } from "./models/grupo.js";
 import "./models/mongo.js";
 import { ObraModel } from "./models/obra.js";
@@ -10,7 +14,22 @@ import { createObraRouterV1 } from "./routes/obrasV1.js";
 import { createPlanoRouterV1 } from "./routes/planosV1.js";
 import { createRepresentacionRouterV1 } from "./routes/representacionesV1.js";
 
+// Configura la instancia de axios con la base URL desde las variables de entorno
+const api = axios.create({
+  baseURL: process.env.API_BASE_URL,
+});
+
 const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: [
+      "http://localhost:4200",
+      "https://entradasteatromenesiano.onrender.com",
+      "https://entradasteatromenesiano-pre.onrender.com",
+    ],
+  },
+});
 
 app.use(json({ limit: "50mb" }));
 app.use(
@@ -33,6 +52,31 @@ app.get("/", (req, res) => {
   res.send("<h1>Servidor despierto!</h1>");
 });
 
+io.on("connection", (socket) => {
+  console.log("Nuevo cliente conectado");
+  socket.on("disconnect", () => {
+    console.log("Cliente desconectado");
+  });
+  socket.on("join", (representacionId) => {
+    console.log("Unido al plano " + representacionId);
+    socket.join(representacionId);
+  });
+  socket.on("butacas", async (plano) => {
+    try {
+      // Hacer la solicitud HTTP a la API REST para actualizar el plano
+      const response = await api.patch(
+        `/api/v1/planos/${plano.representacion}`,
+        plano.butacas
+      );
+
+      // Emitir el evento al resto de los clientes
+      io.to(plano.representacion).emit("butacas", response.data);
+    } catch (error) {
+      console.error("Error al actualizar el plano:", error);
+    }
+  });
+});
+
 app.use((req, res, next) => {
   console.log(req.method, req.url, req.body);
   next();
@@ -53,6 +97,6 @@ app.use(
 app.use("/api/v1/planos", createPlanoRouterV1({ planoModel: PlanoModel }));
 
 const PORT = process.env.PORT || 1993;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
 });
